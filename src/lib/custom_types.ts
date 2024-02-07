c7ad65f04a39a59dafe47f5d22a03d402791a71c
HavitOngit <havitonline@gmail.com>
2024-02-07 21:03:10 +0530

    Type Defined

---
+++ src/lib/custom_types.ts
@@ -0,0 +1,57 @@
+
+export interface Group {
+    id?: number,
+    name: string
+}
+
+export interface Attendance {
+    id?: number,
+    date: Date,
+    boys: number,
+    girls: number,
+    total: number,
+    group_id: number
+}
+
+export interface Storage {
+    id?: number,
+    name: string,
+    amount: number,
+    group_id: number
+}
+
+export interface StorageHistory {
+    id?: number,
+    date: Date,
+    name: string,
+    amount: number,
+    group_id: number
+}
+
+export interface Usage {
+    id?: number,
+    date: Date,
+    name: string,
+    usage_amount: number,
+    past_storage_amount: number,
+    remaining_storage_amount: number,
+    rate: number,
+    group_id: number
+}
+
+export interface Rates {
+    id?: number,
+    day: "Mon" | "Tue" | "Wed" | "Thu" | "Fri" | "Sat" | "Sun",
+    name: string,
+    rate: number,
+    group_id: number
+}
+
+export interface BackUp {
+    id?: number,
+    date: Date,
+    start: Date,
+    end: Date,
+    action: "Upload" | "Download"
+}
+
