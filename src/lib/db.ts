899f700926439088fb0fb0969d73459d4ccbdb6e
HavitOngit <havitonline@gmail.com>
2024-02-08 15:08:03 +0530

    group dependence protecction

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -14,7 +14,7 @@ export class AnajDB extends Dexie {
         super('AnajDB');
 
         this.version(1).stores({
-            group: '++id, name, storage_unit_id',
+            group: '++id, name, storage_unit_id, currently_used',
             attendance: '++id, date, boys, girls, total, group_id',
             storage: '++id, name, amount',
             storage_history: '++id, date, name, amount, storage_unit_id',
