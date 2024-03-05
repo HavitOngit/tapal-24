5400a1b6a90646da7307fb8b8eea146d2be0aa5b
HavitOngit <havitonline@gmail.com>
2024-03-05 13:36:13 +0530

    ..

--- src/routes/rates/[id]/+page.svelte
+++ src/routes/rates/[id]/+page.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { getAllUsedAnajs } from '$lib/api';
 	import RateView from '$lib/components/RateView.svelte';
 	import Rateslot from '$lib/components/Rateslot.svelte';
 	import type { Rate } from '$lib/custom_types';
 	import { db } from '$lib/db';
 	import { liveQuery } from 'dexie';
+	import { onMount } from 'svelte';
 	import { parse } from 'svelte/compiler';
 
 	const id = parseInt($page.params.id);
 
 	const list = liveQuery(() => db.rate.where({ rate_unit_id: id }).toArray());
+
+	onMount(async () => {
+		console.log(await getAllUsedAnajs(id));
+	});
 </script>
 
 {$page.params.id}
