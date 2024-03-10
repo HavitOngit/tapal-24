0c507bf22896234f69493bc8110a8d6aeab0afb3
HavitOngit <havitonline@gmail.com>
2024-03-10 19:48:10 +0530

    pwa t1

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,38 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 import { defineConfig } from 'vite';
 
+
+const manifest = {
+	"short_name": "tapal-24",
+	"name": "tapal-24",
+	"icons": [
+		{
+			"src": "/favicon.png",
+			"sizes": "512x512",
+			"type": "image/png"
+		}
+	],
+	"id": "/",
+	"start_url": "/",
+	"background_color": "#bbdefb",
+
+	"scope": "/",
+	"theme_color": "#bbdefb",
+	"shortcuts": [
+		{
+			"name": "+ Create",
+			"short_name": "Create",
+			"description": "Create a new Board",
+			"url": "/?source=pwa",
+
+		}
+	],
+	"description": "Tapal 24",
+
+}
+
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(),
+	SvelteKitPWA({ manifest: manifest })]
 });
