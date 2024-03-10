28dddba8311caa1e84b9055fe9e0e36279e90007
HavitOngit <havitonline@gmail.com>
2024-03-11 04:28:52 +0530

    Revert "pwa t1"
    This reverts commit 0c507bf22896234f69493bc8110a8d6aeab0afb3.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 	import Badge from '$lib/components/ui/badge/badge.svelte';
 </script>
 
-<div class="flex h-12 w-full items-center justify-center bg-blue-100">
+<div class="flex h-11 w-full items-center justify-center bg-slate-600">
 	<div class="absolute left-0 justify-start">
 		<button
 			on:click={() => {
@@ -14,12 +14,12 @@
 			}}
 			class="flex"
 		>
-			<Badge variant="outline" class="">
+			<Badge variant="outline" class=" text-white">
 				<ChevronLeft></ChevronLeft>Back
 			</Badge>
 		</button>
 	</div>
-	<a href="/">Home</a>
+	<a href="/" class="text-yellow-200">Home</a>
 </div>
 {#if $navigating}
 	<div class="flex flex-col items-center gap-1">
