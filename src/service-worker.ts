c8a7fab19370f9b91a1ac01401032985661cae1f
HavitOngit <havitonline@gmail.com>
2024-04-28 15:19:13 +0530

    docs service-worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,47 +1,81 @@
-/// <reference types="@sveltejs/kit"/>
+/// <reference types="@sveltejs/kit" />
 /// <reference lib="webworker"/>
-
-declare let self: ServiceWorkerGlobalScope;
-
 import { build, files, version } from '$service-worker';
 
-const ASSETS = [...build, ...files];
+const CACHE = `cache-${version}`;
 
-
-// install
+const ASSETS = [
+    ...build, // the app itself
+    ...files  // everything in `static`
+];
 
 self.addEventListener('install', (event) => {
-    async function addfilestocache() {
-        const cache = await caches.open(version);
+    // Create a new cache and add all files to it
+    async function addFilesToCache() {
+        const cache = await caches.open(CACHE);
         await cache.addAll(ASSETS);
     }
 
-    event.waitUntil(addfilestocache());
-
+    event.waitUntil(addFilesToCache());
 });
 
-
-// activate
-
 self.addEventListener('activate', (event) => {
-    async function removeoldcaches() {
-        const keys = await caches.keys();
-        for (const key of keys) {
-            if (key !== version) await caches.delete(key);
+    // Remove previous cached data from disk
+    async function deleteOldCaches() {
+        for (const key of await caches.keys()) {
+            if (key !== CACHE) await caches.delete(key);
         }
     }
 
-    event.waitUntil(removeoldcaches());
+    event.waitUntil(deleteOldCaches());
 });
-
-// fetch
 
 self.addEventListener('fetch', (event) => {
-    async function fetchfromcache() {
-        const cachedresponse = await caches.match(event.request);
-        if (cachedresponse) return cachedresponse;
-        return fetch(event.request);
+    // ignore POST requests etc
+    if (event.request.method !== 'GET') return;
+
+    async function respond() {
+        const url = new URL(event.request.url);
+        const cache = await caches.open(CACHE);
+
+        // `build`/`files` can always be served from the cache
+        if (ASSETS.includes(url.pathname)) {
+            const response = await cache.match(url.pathname);
+
+            if (response) {
+                return response;
+            }
+        }
+
+        // for everything else, try the network first, but
+        // fall back to the cache if we're offline
+        try {
+            const response = await fetch(event.request);
+
+            // if we're offline, fetch can return a value that is not a Response
+            // instead of throwing - and we can't pass this non-Response to respondWith
+            if (!(response instanceof Response)) {
+                throw new Error('invalid response from fetch');
+            }
+
+            if (response.status === 200) {
+                cache.put(event.request, response.clone());
+            }
+
+            return response;
+        } catch (err) {
+            const response = await cache.match(event.request);
+
+            if (response) {
+                return response;
+            }
+
+            // if there's no cache, then just error out
+            // as there is nothing we can do to respond to this request
+            throw err;
+        }
     }
 
-    event.respondWith(fetchfromcache());
+    event.respondWith(respond());
 });
+
