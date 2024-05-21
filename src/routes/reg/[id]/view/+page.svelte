bec2bfc5b07c95234a3540e8114032e1e611cd1b
HavitOngit <havitonline@gmail.com>
2024-05-22 04:18:17 +0530

    need good testing

--- src/routes/reg/[id]/view/+page.svelte
+++ src/routes/reg/[id]/view/+page.svelte
@@ -1,7 +1,42 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import UpdateEntry from '$lib/components/extraFeatures/UpdateEntry.svelte';
+	import { TableBody } from '$lib/components/ui/table';
+	import TableHead from '$lib/components/ui/table/table-head.svelte';
+	import TableHeader from '$lib/components/ui/table/table-header.svelte';
+	import TableRow from '$lib/components/ui/table/table-row.svelte';
+	import Table from '$lib/components/ui/table/table.svelte';
+	import { db } from '$lib/db';
+	import { liveQuery } from 'dexie';
+
+	const usage = liveQuery(() => db.usage.where({ group_id: Number($page.params.id) }).toArray());
+	$: console.log($usage);
 </script>
 
-{$page.params.id}
-<UpdateEntry group_id={Number($page.params.id)}></UpdateEntry>
+<!-- {$page.params.id}
+<UpdateEntry group_id={Number($page.params.id)}></UpdateEntry> -->
+
+{#if $usage}
+	<Table>
+		<TableHeader>
+			<TableRow>
+				<TableHead>Date</TableHead>
+				<TableHead>Name</TableHead>
+				<TableHead>Usage</TableHead>
+				<TableHead>Before</TableHead>
+				<TableHead>After</TableHead>
+			</TableRow>
+		</TableHeader>
+		<TableBody>
+			{#each $usage as item}
+				<TableRow>
+					<TableHead>{item.date.toLocaleDateString()}</TableHead>
+					<TableHead>{item.name}</TableHead>
+					<TableHead>{item.amount}</TableHead>
+					<TableHead>{item.before_amount}</TableHead>
+					<TableHead>{item.after_amount}</TableHead>
+				</TableRow>
+			{/each}
+		</TableBody>
+	</Table>
+{/if}
