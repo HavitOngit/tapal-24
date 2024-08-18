7cc0d861166661c25ceaeb50443606e054b4aa49
HavitOngit <havitonline@gmail.com>
2024-08-18 16:12:11 +0530

    t2

--- src/lib/onlineDB/onlineDB.ts
+++ src/lib/onlineDB/onlineDB.ts
@@ -1,12 +1,13 @@
 import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client/web';
+import { env } from '$env/dynamic/private';
 
 config({ path: '.env' }); // or .env.local
 
 const client = createClient({
-    url: process.env.TURSO_CONNECTION_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
+    url: env.TURSO_CONNECTION_URL,
+    authToken: env.TURSO_AUTH_TOKEN,
 });
 
 export const DB = drizzle(client);
