078d182869a92abafa3de48c911929de83030dce
HavitOngit <havitonline@gmail.com>
2024-08-18 16:33:25 +0530

    creating db from server pages

--- src/lib/onlineDB/onlineDB.ts
+++ src/lib/onlineDB/onlineDB.ts
@@ -1,12 +1,12 @@
 import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client/web';
+import { env } from '$env/dynamic/private';
 
 config({ path: '.env' }); // or .env.local
 
-const client = () => createClient({
-    url: process.env.TURSO_CONNECTION_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
+export const client = () => createClient({
+    url: env.TURSO_CONNECTION_URL!,
+    authToken: env.TURSO_AUTH_TOKEN!,
 });
 
-export const DB = drizzle(client());
--- src/routes/test/+page.server.ts
+++ src/routes/test/+page.server.ts
@@ -1,9 +1,11 @@
 import type { PageServerLoad } from './$types';
-import { DB } from '$lib/onlineDB/onlineDB';
+
 import { type Todo } from '$lib/onlineDB/schema';
 import { todo } from '$lib/onlineDB/schema';
+import { drizzle } from 'drizzle-orm/libsql';
+import { client } from '$lib/onlineDB/onlineDB';
 
-
+const DB = drizzle(client());
 export const load = (async () => {
     const entries = await DB.select().from(todo)
     const ss = await DB.insert(todo).values({ title: 'test' });
