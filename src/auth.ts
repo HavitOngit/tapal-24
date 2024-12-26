3fc3a95c09ddd9666251639ed71d214988414187
HavitOngit <havitonline@gmail.com>
2024-12-26 20:15:37 +0530

    t4

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,8 @@
 
 import { SvelteKitAuth } from "@auth/sveltekit"
-import Google from "@auth/sveltekit/providers/google"
+// import Google from "@auth/sveltekit/providers/google"
 import Github from "@auth/sveltekit/providers/github"
 export const { handle, signIn, signOut } = SvelteKitAuth({
     trustHost: true,
-    providers: [Google, Github],
+    providers: [Github],
 }) 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,62 @@
-hi
+<script>
+	import { navigating } from '$app/stores';
+	// import Accpop from '$lib/components/extraFeatures/accpop.svelte';
+	import LoadingBar from '$lib/components/extraFeatures/loadingBar.svelte';
+	// import MobileConsole from '$lib/components/extraFeatures/mobile-console.svelte';
+	import NavigationBar from '$lib/finalize/NavigationBar.svelte';
+	import { onMount } from 'svelte';
+	import { Toaster } from 'svelte-french-toast';
+	import '../app.pcss';
+	// translations
+	import { dev } from '$app/environment';
+	import en from '$lib/locales/en.json';
+	import gu from '$lib/locales/gu.json';
+	import hi from '$lib/locales/hi.json';
+	import { addMessages, init } from 'svelte-intl-precompile';
+	addMessages('gu', gu);
+	addMessages('en', en);
+	addMessages('hi', hi);
+
+	onMount(() => {
+		const lang = localStorage.getItem('locale') || localStorage.setItem('locale', 'gu');
+		navigator.serviceWorker.register('/service-worker.js', { type: dev ? 'module' : 'classic' });
+		init({
+			initialLocale: String(lang),
+			fallbackLocale: 'en'
+		});
+	});
+</script>
+
+<div class=" my-1 flex h-12 w-full items-center justify-between bg-white p-2">
+	<div class=" justify-start">
+		<!-- <button
+			on:click={() => {
+				history.back();
+			}}
+			class="flex"
+		>
+			<Badge variant="outline" class=" text-black">
+				<ChevronLeft></ChevronLeft>{$t('Back')}</Badge
+			>
+		</button> -->
+		<p class="text-xl font-semibold text-blue-400">MDM <span class="text-sm"> assistant </span></p>
+	</div>
+	<!-- <a href="/" class="text-black">{$t('Home')}</a> -->
+	<div class="flex items-center gap-3">
+		<!-- <LucideCloudOff></LucideCloudOff> -->
+		<!-- <Accpop></Accpop> -->
+		<!-- <Headertwo></Headertwo> -->
+	</div>
+</div>
+<hr />
+<Toaster></Toaster>
+{#if $navigating}
+	<LoadingBar></LoadingBar>
+{:else}
+	<slot />
+{/if}
+<!-- <MobileConsole></MobileConsole> -->
+<div class="h-40"></div>
+<div class="fixed bottom-0">
+	<NavigationBar></NavigationBar>
+</div>
