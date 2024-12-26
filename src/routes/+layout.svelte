ed710ac11a5168f49fb49717e1132950b2f3f8fb
HavitOngit <havitonline@gmail.com>
2024-12-26 16:53:39 +0530

    t3

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,20 +2,16 @@
 	import { navigating } from '$app/stores';
 	import Accpop from '$lib/components/extraFeatures/accpop.svelte';
 	import LoadingBar from '$lib/components/extraFeatures/loadingBar.svelte';
-	import MobileConsole from '$lib/components/extraFeatures/mobile-console.svelte';
-	import Badge from '$lib/components/ui/badge/badge.svelte';
 	import NavigationBar from '$lib/finalize/NavigationBar.svelte';
-	import { ChevronLeft, Cloud, LucideCloudOff } from 'lucide-svelte';
 	import { onMount } from 'svelte';
 	import { Toaster } from 'svelte-french-toast';
-	import { t } from 'svelte-intl-precompile';
 	import '../app.pcss';
 	// translations
+	import { dev } from '$app/environment';
 	import en from '$lib/locales/en.json';
 	import gu from '$lib/locales/gu.json';
 	import hi from '$lib/locales/hi.json';
 	import { addMessages, init } from 'svelte-intl-precompile';
-	import { dev } from '$app/environment';
 
 	addMessages('gu', gu);
 	addMessages('en', en);
@@ -51,7 +47,7 @@
 		<Accpop></Accpop>
 		<!-- <Headertwo></Headertwo> -->
 	</div>
-</div> 
+</div>
 <hr />
 <Toaster></Toaster>
 {#if $navigating}
@@ -59,7 +55,7 @@
 {:else}
 	<slot />
 {/if}
-<MobileConsole></MobileConsole>
+
 <div class="h-40"></div>
 <div class="fixed bottom-0">
 	<NavigationBar></NavigationBar>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,25 @@
 <script lang="ts">
-import { t } from 'svelte-intl-precompile';
-	import CreateStorage from '$lib/components/CreateStorage.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
-	import { db } from '$lib/db';
+	import { t } from 'svelte-intl-precompile';
 
 	let name: string;
 </script>
 
-                                                                                                                                                                                                           
-
-                                        
 <div>
-	<a href="/stocks">{$t("stocks")}</a>
+	<a href="/stocks">{$t('stocks')}</a>
 </div>
 <div>
-	<a href="/rates">{$t("rates")}</a>
-</div>
-<div>``
-	<a href="/reg">{$t("Registers")}</a>
+	<a href="/rates">{$t('rates')}</a>
 </div>
 <div>
-	<a href="/dailywork">{$t("DailyWork")}</a>
+	``
+	<a href="/reg">{$t('Registers')}</a>
 </div>
 <div>
-	<a href="/backup">{$t("BackUp")}</a>
+	<a href="/dailywork">{$t('DailyWork')}</a>
+</div>
+<div>
+	<a href="/backup">{$t('BackUp')}</a>
 </div>
 <Button
 	class="mt-[95%]"
