5501ebb32f66deccc3c5219e8b5a4dbacb0e874b
HavitOngit <havitonline@gmail.com>
2024-12-26 19:35:34 +0530

    t3

--- src/auth.ts
+++
@@ -1,8 +0,0 @@
-
-import { SvelteKitAuth } from "@auth/sveltekit"
-import Google from "@auth/sveltekit/providers/google"
-import Github from "@auth/sveltekit/providers/github"
-export const { handle, signIn, signOut } = SvelteKitAuth({
-    trustHost: true,
-    providers: [Google, Github],
-}) 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,22 +2,17 @@
 	import { navigating } from '$app/stores';
 	import Accpop from '$lib/components/extraFeatures/accpop.svelte';
 	import LoadingBar from '$lib/components/extraFeatures/loadingBar.svelte';
-	import MobileConsole from '$lib/components/extraFeatures/mobile-console.svelte';
-	import Badge from '$lib/components/ui/badge/badge.svelte';
+	// import MobileConsole from '$lib/components/extraFeatures/mobile-console.svelte';
 	import NavigationBar from '$lib/finalize/NavigationBar.svelte';
-	import { ChevronLeft, Cloud, LucideCloudOff } from 'lucide-svelte';
 	import { onMount } from 'svelte';
 	import { Toaster } from 'svelte-french-toast';
-	import { t } from 'svelte-intl-precompile';
 	import '../app.pcss';
 	// translations
+	import { dev } from '$app/environment';
 	import en from '$lib/locales/en.json';
 	import gu from '$lib/locales/gu.json';
 	import hi from '$lib/locales/hi.json';
 	import { addMessages, init } from 'svelte-intl-precompile';
-	import { dev } from '$app/environment';
-	import Headertwo from '$lib/finalize/headertwo.svelte';
-	import * as cDB from '$lib/changesDB';
 	addMessages('gu', gu);
 	addMessages('en', en);
 	addMessages('hi', hi);
@@ -49,7 +44,7 @@
 	<!-- <a href="/" class="text-black">{$t('Home')}</a> -->
 	<div class="flex items-center gap-3">
 		<!-- <LucideCloudOff></LucideCloudOff> -->
-		<Accpop></Accpop>
+		<!-- <Accpop></Accpop> -->
 		<!-- <Headertwo></Headertwo> -->
 	</div>
 </div>
@@ -60,7 +55,7 @@
 {:else}
 	<slot />
 {/if}
-<MobileConsole></MobileConsole>
+<!-- <MobileConsole></MobileConsole> -->
 <div class="h-40"></div>
 <div class="fixed bottom-0">
 	<NavigationBar></NavigationBar>
